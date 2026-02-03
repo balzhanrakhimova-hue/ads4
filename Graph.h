b1fcb3b3d8e6a0c85f939e487da404e4e47d5a5e
balzhanrakhimova-hue <balzhanrakhimova@gmail.com>
2026-02-03 17:45:10 +0500

    Create Graph.h

---
+++ Graph.h
@@ -0,0 +1,50 @@
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class Graph {
+private:
+    int n;
+    bool directed;
+    vector<vector<int>> adj;
+    int edges;
+
+public:
+    Graph(int n, bool directed);
+
+    void addEdge(int u, int v);
+    void removeEdge(int u, int v);
+
+    vector<int> neighbors(int u) const;
+
+    int V() const;
+    int E() const;
+
+    vector<vector<int>> adjacencyMatrix() const;
+
+    void BFS(int start,
+             vector<int>& order,
+             vector<int>& dist,
+             vector<int>& parent) const;
+
+    vector<int> shortestPathUnweighted(int s, int t) const;
+
+    void DFS_recursive(int start, vector<int>& order) const;
+    void DFS_iterative(int start, vector<int>& order) const;
+
+    bool hasCycleDirected() const;
+
+private:
+    void dfsRecUtil(int v, vector<bool>& visited, vector<int>& order) const;
+    bool cycleUtil(int v, vector<int>& color) const;
+};
+
+#endif
